2166d7b2cb13ed82a748b3a03fa9ff25f2192cf2
Priyanshu Tiwari <you@example.com>
2022-01-13 21:34:25 +0530

    +1

---
+++ Java/Recursion/SubsequenceOfString.java
@@ -0,0 +1,28 @@
+public class SubsequenceOfString {
+    public static void main(String[] args) {
+        String str = "abc";
+        // subsequenceOfString(str, "");
+        subsequenceOfStringWithAscii(str,"");
+    }
+
+    private static void subsequenceOfString(String str, String req) {
+        if (str.isEmpty()) {
+            System.out.println(req);
+            return;
+        }
+        subsequenceOfString(str.substring(1),req+str.substring(0, 1));
+        subsequenceOfString(str.substring(1), req);
+
+    }
+    private static void subsequenceOfStringWithAscii(String str, String req) {
+        if (str.isEmpty()) {
+            System.out.println(req);
+            return;
+        }
+        subsequenceOfStringWithAscii(str.substring(1),req+str.substring(0, 1));
+        subsequenceOfStringWithAscii(str.substring(1),req+(int)str.charAt(0));
+        subsequenceOfStringWithAscii(str.substring(1), req);
+
+    }
+
+}
