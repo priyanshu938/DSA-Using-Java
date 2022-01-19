3914540b40ad0978591044e9aac52f3c1d6ec1f9
Priyanshu Tiwari <you@example.com>
2022-01-19 23:28:23 +0530

    +1

---
+++ Java/Recursion/LetterCombinationsPhoneNo.java
@@ -0,0 +1,18 @@
+public class LetterCombinationsPhoneNo {
+    public static void main(String[] args) {
+        letterCombinations("", "9234");
+    }
+
+    private static void letterCombinations(String p, String up) {
+        if (up.isEmpty()) {
+            System.out.println(p);
+            return;
+        }
+        int digit = up.charAt(0) - '0'; // this will convert '2' into 2
+        for (int i = (digit - 1) * 3; i < digit * 3 && i != 26; i++) // skipping for 26th character which is after z
+        {
+            char ch = (char) ('a' + i);
+            letterCombinations(p + ch, up.substring(1));
+        }
+    }
+}
