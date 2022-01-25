932d5e6b7eb5fe5428d7729f80b7d8095d52b3cc
Priyanshu Tiwari <you@example.com>
2022-01-25 08:14:38 +0530

    +1

--- Java/Recursion/CountingMazePaths.java
+++ Java/Recursion/CountingMazePaths.java
@@ -1,10 +1,47 @@
 import java.util.*;
+
 public class CountingMazePaths {
     public static void main(String[] args) {
         System.out.println(countMaze(3, 3));
         printMazePath(3, 3, "");
         System.out.println(returnPrintMazePathList(3, 3, ""));
         System.out.println(returnDiagVertHorPath(3, 3, ""));
+        boolean[][] board = {
+                { true, true, true },
+                { true, false, true },
+                { true, true, true }
+
+        };
+        restrictionPath(0, 0, board, "");
+        restrictionPathDiagonalMoveAllowed(0, 0, board, "");
+    }
+
+    private static void restrictionPath(int r, int c, boolean[][] board, String s) {
+        if (r == board.length - 1 && c == board[0].length - 1) {
+            System.out.println(s);
+            return;
+        }
+        if (!board[r][c])
+            return;
+        if (r < board.length - 1)
+            restrictionPath(r + 1, c, board, s + "D");
+        if (c < board[0].length - 1)
+            restrictionPath(r, c + 1, board, s + "R");
+    }
+
+    private static void restrictionPathDiagonalMoveAllowed(int r, int c, boolean[][] board, String s) {
+        if (r == board.length - 1 && c == board[0].length - 1) {
+            System.out.println(s);
+            return;
+        }
+        if (!board[r][c])
+            return;
+        if (r < board.length - 1 && c < board[0].length - 1)
+            restrictionPathDiagonalMoveAllowed(r + 1, c + 1, board, s + "D");
+        if (r < board.length - 1)
+            restrictionPathDiagonalMoveAllowed(r + 1, c, board, s + "V");
+        if (c < board[0].length - 1)
+            restrictionPathDiagonalMoveAllowed(r, c + 1, board, s + "H");
     }
 
     private static List<String> returnDiagVertHorPath(int r, int c, String s) {
