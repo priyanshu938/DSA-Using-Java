feb1d48369ea7aeb54261fee53fd83d972fbe52f
Priyanshu Tiwari <you@example.com>
2022-01-28 21:11:27 +0530

    nqueens

---
+++ Java/Recursion/Backtracking/NQueens.java
@@ -0,0 +1,55 @@
+public class NQueens {
+    public static void main(String[] args) {
+        int n = 8;
+        boolean[][] board = new boolean[n][n];
+        nQueens(board, 0);
+    }
+
+    private static void nQueens(boolean[][] board, int row) {
+        if (row == board.length) {
+            display(board);
+            System.out.println();
+            return;
+        }
+        for (int col = 0; col < board.length; col++) {
+            if (isSafe(board, row, col)) {
+                board[row][col] = true;
+                nQueens(board, row + 1);
+                board[row][col] = false;
+            }
+        }
+    }
+
+    private static boolean isSafe(boolean[][] board, int row, int col) {
+        // vertical
+        for (int i = 0; i < row; i++) {
+            if (board[i][col])
+                return false;
+        }
+        // left diagonal
+        int maxLeft = Math.min(row, col);
+        for (int i = 1; i <= maxLeft; i++) {
+            if (board[row - i][col - i])
+                return false;
+        }
+        // right diagonal
+        int maxRight = Math.min(row, board.length - col - 1);
+        for (int i = 1; i <= maxRight; i++) {
+            if (board[row - i][col + i])
+                return false;
+        }
+        return true;
+    }
+
+    private static void display(boolean[][] board) {
+        for (boolean[] row : board) {
+            for (boolean col : row) {
+                if (col == true)
+                    System.out.print("Q ");
+                else
+                    System.out.print(". ");
+            }
+            System.out.println();
+        }
+    }
+}
