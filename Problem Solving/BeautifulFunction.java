2dc270db1c77ee48c4b2ec4e27f4568d92cad290
Priyanshu Tiwari <you@example.com>
2022-02-03 15:53:14 +0530

    +1

--- Problem Solving/BeautifulFunction.java
+++ Problem Solving/BeautifulFunction.java
@@ -1,4 +1,5 @@
 import java.util.*;
+
 public class BeautifulFunction {
     public static int count_trailing_zeroes(int num) {
         int c = 0;
@@ -21,6 +22,7 @@ public class BeautifulFunction {
         else
             return num / (int) Math.pow(10, count);
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
@@ -33,7 +35,7 @@ public class BeautifulFunction {
                 break;
             stack.push(beautiful);
         }
-        System.out.println(n==1 ? stack.size() : stack.size() + 1);
+        System.out.println(n == 1 ? stack.size() : stack.size() + 1);
         scanner.close();
     }
 }
--- Problem Solving/MinimumWithdrawls.java
+++ Problem Solving/MinimumWithdrawls.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MinimumWithdraw {
+public class MinimumWithdrawls {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
