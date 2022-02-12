aaa6fb866a7ef72f540921247646a4a229aa1c59
Priyanshu Tiwari <you@example.com>
2022-02-12 21:56:59 +0530

    +1

--- Java/Linked List/tempCodeRunnerFile.java
+++
@@ -1,6 +0,0 @@
-insert(10, 0);
-        // System.out.println("Linked list after insertion is : ");
-        // traverse();
-        // delete(0);
-        // System.out.println("Linked list after deletion is : ");
-        // traverse();
