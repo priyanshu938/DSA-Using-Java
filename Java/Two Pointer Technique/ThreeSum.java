e6407d9a4a000d0e23243ef9ead1969fadd59cc8
Priyanshu Tiwari <you@example.com>
2022-03-02 18:03:51 +0530

    three sum and two sum

--- Java/Two Pointer Technique/ThreeSum.java
+++ Java/Two Pointer Technique/ThreeSum.java
@@ -2,9 +2,38 @@ import java.util.*;
 
 public class ThreeSum {
     public static void main(String[] args) {
-        int[] nums = { -1, 0, 1, 2, -1, 4 };
+        int[] nums = { -1, 0, 1, 2, -1, -4 };
         int target = 0;
         System.out.println(threeSumBruteForce(nums, target));
+        System.out.println(threeSumUsingTwoPointerApproach(nums, target));
+
+    }
+
+    private static List<List<Integer>> threeSumUsingTwoPointerApproach(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList<List<Integer>>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1])) {
+                int a = 0 - nums[i];
+                int s = i + 1, e = nums.length - 1;
+                while (s < e) {
+                    int b = nums[s], c = nums[e];
+                    if (b + c == a) {
+                        res.add(Arrays.asList(-a, b, c));
+                        while (s < e && nums[s] == nums[s + 1])
+                            s++;
+                        while (s < e && nums[e] == nums[e - 1])
+                            e--;
+                        s++;
+                        e--;
+                    } else if (b + c > a)
+                        e--;
+                    else
+                        s++;
+                }
+            }
+        }
+        return res;
     }
 
     private static List<List<Integer>> threeSumBruteForce(int[] nums, int target) {
--- Java/Two Pointer Technique/TwoSum.java
+++ Java/Two Pointer Technique/TwoSum.java
@@ -3,10 +3,28 @@ import java.util.*;
 public class TwoSum {
     public static void main(String[] args) {
         int[] nums = { 2, 7, 11, 15 };
-        int target = 17;
+        int target = 9;
         System.out.println(twoSumBruteForceQuadraticTimeComplexity(nums, target));
         System.out.println(twoSumUsingHashMapLinearTimeComplexity(nums, target));
+        System.out.println(twoSumUsingTwoPointersApproach(nums, target));
+    }
 
+    private static List<Integer> twoSumUsingTwoPointersApproach(int[] nums, int target) {
+        int s = 0, e = nums.length - 1;
+        Arrays.sort(nums);
+        List<Integer> ans = new LinkedList<Integer>();
+
+        while (s < e) {
+            if (nums[s] + nums[e] == target) {
+                ans.add(nums[s]);
+                ans.add(nums[e]);
+                break;
+            } else if (nums[s] + nums[e] > target) {
+                e--;
+            } else
+                s++;
+        }
+        return ans;
     }
 
     private static List<Integer> twoSumUsingHashMapLinearTimeComplexity(int[] nums, int target) {
@@ -26,15 +44,14 @@ public class TwoSum {
 
     private static List<Integer> twoSumBruteForceQuadraticTimeComplexity(int[] nums, int target) {
         List<Integer> ans = new LinkedList<Integer>();
-        for (Integer x : nums) {
-            for (Integer y : nums) {
-                if (x + y == target) {
-                    ans.add(x);
-                    ans.add(y);
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans.add(nums[i]);
+                    ans.add(nums[j]);
                     return ans;
                 }
             }
-
         }
         return ans;
     }
