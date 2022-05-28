23909f274c8165fbb0f4d2ae38b1055233def9c9
Priyanshu_Tiwari <priyanshutiwari6789@gmail.com>
2022-05-28 14:22:22 +0530

    bfs of graph

---
+++ Java/Graphs/BFS.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class BFS {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int V = sc.nextInt(), E = sc.nextInt(), i, u, v;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>();
+        for (i = 1; i <= V+1; i++)
+            adj.add(new ArrayList<Integer>());
+
+        for (i = 1; i <= E; i++) {
+            u = sc.nextInt();
+            v = sc.nextInt();
+            adj.get(u).add(v);
+            // adj.get(v).add(u);
+        }
+        System.out.println(bfsOfGraph(V, adj));
+
+    }
+
+    public static ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean vis[] = new boolean[V + 1];
+        ArrayList<Integer> bfs = new ArrayList<>();
+        for (int i = 1; i <= V; i++) {
+            if (vis[i] == false) {
+                Queue<Integer> q = new LinkedList<>();
+                q.offer(i);
+                vis[i] = true;
+                while (!q.isEmpty()) {
+                    Integer node = q.poll();
+                    bfs.add(node);
+                    for (Integer it : adj.get(node)) {
+                        if (vis[it] == false) {
+                            bfs.add(it);
+                            vis[it] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return bfs;
+    }
+}
