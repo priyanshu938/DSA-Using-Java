f8a8398845414f5ec37cbd40fc2c2fa5d77b3442
Priyanshu_Tiwari <priyanshutiwari6789@gmail.com>
2022-06-01 11:24:06 +0530

    +1

--- Java/Graphs/ShortestPathUndirectedWeightedGraph.java
+++ Java/Graphs/ShortestPathUndirectedWeightedGraph.java
@@ -1,12 +1,22 @@
+
 //Dijkstra's algorithm
+/* 
+We can implement the algorithm using the following steps:
+1) We would be using a min-heap and a distance array of size N initialized with infinity (indicating that at present none of the nodes are reachable from the source node) and initialize the distance to source node as 0.
+2) We push the source node to the queue.
+3) For every node at the top of the queue we pop that element out and look out for its adjacent nodes. If the current reachable distance is better than the previous distance indicated by the distance array, we update the distance and push it in the queue.
+4) A node with a lower distance would be at the top of the priority queue as opposed to a node with a higher distance. By following the steps 3, until our queue becomes empty, we would get the minimum distance from the source node to all other nodes. Here’s a quick demonstration of the same.
+*/
 import java.util.*;
 
 class Node implements Comparator<Node> {
     private int v, w;
-//Constructor 1
+
+    // Constructor 1
     Node() {
     }
-//Constructor 2
+
+    // Constructor 2
     Node(int v, int w) {
         this.v = v;
         this.w = w;
@@ -38,7 +48,7 @@ public class ShortestPathUndirectedWeightedGraph {
             int V = scanner.nextInt();
             int E = scanner.nextInt(), u, v, w, i;
             ArrayList<ArrayList<Node>> adj = new ArrayList<ArrayList<Node>>();
-            for (i = 1; i <= V + 1; i++) {
+            for (i = 0; i <= V; i++) {
                 adj.add(new ArrayList<Node>());
             }
             for (i = 1; i <= E; i++) {
@@ -54,10 +64,14 @@ public class ShortestPathUndirectedWeightedGraph {
     }
 
     private static void shortestPathUndirectedWeightedGraph(int src, ArrayList<ArrayList<Node>> adj, int V) {
-        int[] dis = new int[V + 1];
+        int[] dis = new int[V];
         Arrays.fill(dis, Integer.MAX_VALUE);
         dis[src] = 0;
-        PriorityQueue<Node> pq = new PriorityQueue<Node>(V, new Node());// size of PQ is V and type is Node
+        PriorityQueue<Node> pq = new PriorityQueue<Node>(new Node());
+        /*
+         * new Node() declares a custom comparator that was built in Node class, i.e.,
+         * converting the priority queue to a min-Heap priority queue.
+         */
         pq.offer(new Node(src, 0));
         while (!pq.isEmpty()) {
             Node node = pq.poll();
